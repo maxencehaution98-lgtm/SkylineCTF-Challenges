bcc72e6f020fee74c47c0c9717d112395cf65fb5
Sp00kySkelet0n <spookymail>
2026-02-11 22:11:00 +0100

    Add source of StackOverflow

---
+++ Stack-Overflow/uploads/vuln.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_flag() {
+    char flag[128];
+    FILE *f = fopen("/flag.txt", "r");
+    if (f) {
+        fgets(flag, sizeof(flag), f);
+        fclose(f);
+        printf("Accès autorisé !\n%s\n", flag);
+    } else {
+        printf("Erreur: flag introuvable.\n");
+    }
+}
+
+int main() {
+    struct {
+        char buffer[32];
+        int authorized;
+    } data;
+
+    data.authorized = 0;
+
+    printf("=== SKL Corp - Terminal d'accès ===\n");
+    printf("Identifiant : ");
+    fflush(stdout);
+
+    gets(data.buffer);
+
+    if (data.authorized != 0) {
+        print_flag();
+    } else {
+        printf("Accès refusé.\n");
+    }
+
+    return 0;
+}
